cdf5fe960cc5df616abc5b245d338672330d149c
LouisMS <louisms@gmail.com>
2017-01-24 10:47:57 +0000

    update to how csv files are read

--- CleanUP.R
+++ CleanUP.R
@@ -1,24 +1,27 @@
-#install and activate dplyr and tidyr
-install.packages("dplyr")
-install.packages("tidyr")
+# Install and activate dplyr and tidyr (remove #s below to install packages if not already installed)
+# install.packages("dplyr")
+# install.packages("tidyr")
 
 library("dplyr")
 library("tidyr")
 
-#read raw files players.csv and match.csv into project
+# Read raw files players.csv and match.csv into project
 
 players <- read.csv("players.csv")
 match <- read.csv("match.csv")
 
-#clean match.csv file and collect key infomation into one data frame
+# Clean match.csv file and collect key infomation into one data frame
 
 MatchDF <- match %>% select(1 : 5, 10)
 
-#clean players.csv file and collect key infomation into one data frame
+# Clean players.csv file and collect key infomation into one data frame
 
 PlayersDF <- players %>% select(1 : 17)
 
-#convert start time from seconds to date in MatchDF
+# Convert start time from seconds to date in MatchDF
 
 MatchDF <- MatchDF %>% mutate(date = as.POSIXct(MatchDF$start_time, tz = "UTC", origin = "1970-01-01"))
 
+# Combine PlayersDF and MatchDF into CombinedDF
+
+CombinedDF <- merge(PlayersDF, MatchDF, by = "match_id")
